6781e66bc632ef3121b8d00d4d45f21c758d300e
vasilisdmr <vasilisdmr@gmail.com>
2023-01-24 09:50:45 +0200

    Refactor methods to use the conversion methods

--- src/main/kotlin/com/example/krchat/service/PersistentMessageService.kt
+++ src/main/kotlin/com/example/krchat/service/PersistentMessageService.kt
@@ -1,5 +1,7 @@
 package com.example.krchat.service
 
+import com.example.krchat.asDomainObject
+import com.example.krchat.asViewModel
 import com.example.krchat.repository.ContentType
 import com.example.krchat.repository.Message
 import com.example.krchat.repository.MessageRepository
@@ -12,22 +14,18 @@ import java.net.URL
 class PersistentMessageService(val messageRepository: MessageRepository) : MessageService {
     override fun latest(): List<MessageVM> = messageRepository.findLatest()
         .map {
-            with(it) {
-                MessageVM(content, UserVM(username, URL(userAvatarImageLink)), sent, id)
-            }
+            it.asViewModel()
         }
 
     override fun after(messageId: String): List<MessageVM> = messageRepository.findLatest(messageId)
         .map {
-            with(it) {
-                MessageVM(content, UserVM(username, URL(userAvatarImageLink)), sent, id)
-            }
+            it.asViewModel()
         }
 
     override fun post(message: MessageVM) {
         messageRepository.save(
             with(message) {
-                Message(content, ContentType.PLAIN, sent, user.name, user.avatarImageLink.toString())
+                this.asDomainObject()
             }
         )
     }
