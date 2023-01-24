d7f40094fc2595b495c5815a1efb82fed4359cdf
vasilisdmr <vasilisdmr@gmail.com>
2023-01-24 09:44:00 +0200

    Add mapping extension methods

---
+++ src/main/kotlin/com/example/krchat/Extensions.kt
@@ -0,0 +1,29 @@
+package com.example.krchat
+
+import com.example.krchat.repository.ContentType
+import com.example.krchat.repository.Message
+import com.example.krchat.service.MessageVM
+import com.example.krchat.service.UserVM
+import java.net.URL
+
+fun MessageVM.asDomainObject(contentType: ContentType = ContentType.PLAIN): Message =
+    Message(
+        content = this.content,
+        contentType = contentType,
+        sent = this.sent,
+        username = this.user.name,
+        userAvatarImageLink = this.user.avatarImageLink.toString(),
+        id = this.id
+    )
+
+
+fun Message.asViewModel(): MessageVM =
+    MessageVM(
+        content = this.content,
+        user = UserVM(
+            name = this.username,
+            avatarImageLink = URL(this.userAvatarImageLink)
+        ),
+        sent = this.sent,
+        id = this.id
+    )
