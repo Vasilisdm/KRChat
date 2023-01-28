962317257fd1c1a208468dcc1dbefd529dcc74a9
vasilisdmr <vasilisdmr@gmail.com>
2023-01-28 14:36:28 +0200

    Add coroutines

--- src/main/kotlin/com/example/krchat/service/PersistentMessageService.kt
+++ src/main/kotlin/com/example/krchat/service/PersistentMessageService.kt
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service
 @Service
 @Primary
 class PersistentMessageService(val messageRepository: MessageRepository) : MessageService {
-    override fun latest(): List<MessageVM> = messageRepository.findLatest().mapToViewModel()
+    override suspend fun latest(): List<MessageVM> = messageRepository.findLatest().mapToViewModel()
 
-    override fun after(messageId: String): List<MessageVM> = messageRepository.findLatest(messageId).mapToViewModel()
+    override suspend fun after(messageId: String): List<MessageVM> = messageRepository.findLatest(messageId).mapToViewModel()
 
-    override fun post(message: MessageVM) {
+    override suspend fun post(message: MessageVM) {
         messageRepository.save(
             with(message) {
                 this.asDomainObject()
